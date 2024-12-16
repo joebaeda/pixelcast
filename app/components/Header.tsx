ac5c56d27f4df30b6c6fb6256e1363210c46e533
Joe bae <lineanity@gmail.com>
2024-12-16 21:20:18 +0700

    minor fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import { useAccount, useBalance, useChainId } from "wagmi";
+import { useAccount, useBalance } from "wagmi";
 import { formatEther } from 'viem';
 import Ethereum from "./Ethereum";
 
@@ -11,19 +11,15 @@ interface IHeader {
 }
 
 const Header = ({ username, pfp }: IHeader) => {
-    const chainId = useChainId();
     const { address } = useAccount();
-    const balance = useBalance({
-        address: address,
-        chainId: chainId,
-    })
+    const balance = useBalance({ address })
   
     return (
         <div className="fixed rounded-b-2xl mx-auto top-0 left-0 right-0 bg-[#281537] shadow-md p-4">
         <div className="flex text-gray-200 mx-auto flex-row justify-between items-center space-x-4">
             <div className="flex space-x-2 items-center">
                 <Ethereum className="w-10 h-10" />
-                <p className="font-bold"> {address ? parseFloat(formatEther(balance.data?.value as bigint)).toFixed(4) : "0"}</p>
+                <p className="font-bold"> {parseFloat(formatEther(balance.data?.value as bigint)).toFixed(4)}</p>
             </div>
             <div className="flex space-x-2 items-center">
                 <Image className="object-cover w-10 h-10 rounded-full" src={pfp as string} alt={username as string} width={35} height={35} priority />
