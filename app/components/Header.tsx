c2c2c08e7547b3076943c8df9f67cd8ee77ddf06
Joe bae <lineanity@gmail.com>
2024-12-16 23:06:39 +0700

    minor fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,11 +14,11 @@ const Header = ({ username, pfp }: IHeader) => {
         <div className="fixed rounded-b-2xl mx-auto top-0 left-0 right-0 bg-[#281537] shadow-md p-4">
         <div className="flex text-gray-200 mx-auto flex-row justify-between items-center space-x-4">
             <div className="flex space-x-2 items-center">
-                <Ethereum className="w-10 h-10" />
+                <Ethereum className="w-8 h-8" />
             </div>
-            <div className="flex space-x-2 items-center">
-                <Image className="object-cover w-6 h-6 rounded-full" src={pfp as string} alt={username as string} width={35} height={35} priority />
-                <p className="font-bold text-sm">{username}</p>
+            <div className="flex space-x-3 items-center">
+                <Image className="object-cover w-8 h-8 rounded-full" src={pfp as string} alt={username as string} width={35} height={35} priority />
+                <p className="font-bold text-lg">{username}</p>
             </div>
         </div>
     </div>
--- app/components/PixelCast.tsx
+++ app/components/PixelCast.tsx
@@ -2,13 +2,16 @@
 
 import { useState, useRef, useEffect, useCallback } from 'react';
 import PixelGrid from './PixelGrid';
-import ToolBar from './ToolBar';
 import Header from './Header';
 import { useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
 import sdk from '@farcaster/frame-sdk';
 import { pixelCastAbi, pixelCastAddress } from '@/lib/contract';
 import { base } from 'wagmi/chains';
 import { parseEther } from 'viem';
+import { SketchPicker } from 'react-color';
+import { Palette, Trash2 } from 'lucide-react';
+import CastButton from './CastButton';
+import BaseButton from './BaseButton';
 
 interface IProfile {
   fid: number;
@@ -24,7 +27,32 @@ const PixelCast = ({ fid, username, pfp }: IProfile) => {
   const { data: hash, isPending, writeContract } = useWriteContract();
   const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash });
 
-  
+  const [showColorPicker, setShowColorPicker] = useState(false);
+  const [showTransactionModal, setShowTransactionModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isPending) {
+      setModalMessage("Confirming...");
+      setShowTransactionModal(true);
+    } else if (isConfirming) {
+      setModalMessage("Waiting...");
+      setShowTransactionModal(true);
+    } else if (isConfirmed) {
+      setModalMessage("Transaction Confirmed! Success 🎉");
+      setShowTransactionModal(true);
+    } else {
+      setModalMessage(null);
+    }
+
+    if (modalMessage) {
+      const timer = setTimeout(() => {
+        setShowTransactionModal(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [isPending, isConfirming, isConfirmed, modalMessage]);
+
 
   // Notify user
   const notifyUser = useCallback(async (title: string, body: string) => {
@@ -139,16 +167,80 @@ const PixelCast = ({ fid, username, pfp }: IProfile) => {
           selectedColor={selectedColor}
           canvasRef={canvasRef}
         />
-        <ToolBar
-          selectedColor={selectedColor}
-          isPending={isPending}
-          isConfirming={isConfirming}
-          isConfirmed={isConfirmed}
-          onColorChange={handleColorChange}
-          onClearCanvas={handleClearCanvas}
-          onCastImage={handleCast}
-          onBaseImage={handleMint}
-        />
+        {showColorPicker && (
+          <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
+            <div className="flex space-y-4 flex-col bg-white p-4 rounded-md shadow-lg">
+              <SketchPicker
+                color={selectedColor}
+                onChange={(color) => handleColorChange(color.hex)}
+              />
+              <button
+                onClick={() => setShowColorPicker(false)}
+                className="w-full py-2 rounded-2xl bg-blue-500 text-white text-2xl font-semibold hover:bg-blue-700 transition"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+
+        {showTransactionModal && modalMessage && (
+          <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
+            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
+              <p className="text-lg font-bold">{modalMessage}</p>
+            </div>
+          </div>
+        )}
+
+        <div className="fixed rounded-t-2xl mx-auto bottom-0 left-0 right-0 bg-[#281537] shadow-md p-4">
+          <div className="flex mx-auto flex-row justify-between items-center space-x-4">
+
+            <div className="flex flex-col space-y-1 items-center">
+              <button
+                disabled={isConfirming || isPending}
+                onClick={() => setShowColorPicker(true)}
+                className="rounded-full disabled:opacity-50"
+              >
+                <Palette className="w-6 h-6 text-gray-200" />
+              </button>
+              <p className="text-gray-200 text-sm font-bold">Color</p>
+            </div>
+
+            <div className="flex flex-col space-y-1 items-center">
+              <button
+                disabled={isConfirming || isPending}
+                onClick={handleCast}
+                className="rounded-full disabled:opacity-50"
+              >
+                <CastButton className="w-6 h-6" />
+              </button>
+              <p className="text-gray-200 text-sm font-bold">Cast</p>
+            </div>
+
+            <div className="flex flex-col space-y-1 items-center">
+              <button
+                disabled={isConfirming || isPending}
+                onClick={handleMint}
+                className="rounded-full disabled:opacity-50"
+              >
+                <BaseButton className="w-6 h-6" />
+              </button>
+              <p className="text-gray-200 text-sm font-bold">Mint</p>
+            </div>
+
+            <div className="flex flex-col space-y-1 items-center">
+              <button
+                disabled={isConfirming || isPending}
+                onClick={handleClearCanvas}
+                className="rounded-full disabled:opacity-50"
+              >
+                <Trash2 className="w-6 h-6 text-gray-200" />
+              </button>
+              <p className="text-gray-200 text-sm font-bold">Delete</p>
+            </div>
+
+          </div>
+        </div>
       </main>
     </div>
   );
