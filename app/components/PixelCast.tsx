584882a8b32717e34cdc2ad649f209fcd191d9b9
Joe bae <lineanity@gmail.com>
2024-12-17 11:14:00 +0700

    minor fix

--- app/components/PixelCast.tsx
+++ app/components/PixelCast.tsx
@@ -1,234 +1,163 @@
 'use client';
 
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import PixelGrid from './PixelGrid';
 import Header from './Header';
 import { useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
-import sdk from '@farcaster/frame-sdk';
+import sdk, { FrameContext } from '@farcaster/frame-sdk';
 import { pixelCastAbi, pixelCastAddress } from '@/lib/contract';
 import { base } from 'wagmi/chains';
 import { parseEther } from 'viem';
 import { SketchPicker } from 'react-color';
-import { Ellipsis, Palette, Trash2 } from 'lucide-react';
+import { Palette, Trash2 } from 'lucide-react';
 import CastButton from './CastButton';
 import BaseButton from './BaseButton';
+import Loading from './Loading';
+import { Redirect } from './Redirect';
 
-interface IProfile {
-  username: string;
-  pfp: string;
-}
-
-const PixelCast = ({ username, pfp }: IProfile) => {
+const PixelCast = () => {
   const [selectedColor, setSelectedColor] = useState('#000000');
   const canvasRef = useRef<HTMLCanvasElement>(null!);
+
+  // Frame SDK
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [context, setContext] = useState<FrameContext>();
 
   // Wagmi hooks
   const { data: hash, isPending, writeContract } = useWriteContract();
   const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash });
 
   const [showColorPicker, setShowColorPicker] = useState(false);
-  const [showTransactionModal, setShowTransactionModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState<string | null>(null);
+  const [transactionModalMessage, setTransactionModalMessage] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (isPending) {
-      setModalMessage("Confirming...");
-      setShowTransactionModal(true);
-    } else if (isConfirming) {
-      setModalMessage("Waiting...");
-      setShowTransactionModal(true);
-    } else if (isConfirmed) {
-      setModalMessage("Transaction Confirmed! Success 🎉");
-      setShowTransactionModal(true);
-    } else {
-      setModalMessage(null);
-    }
-
-    if (modalMessage) {
-      const timer = setTimeout(() => {
-        setShowTransactionModal(false);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [isPending, isConfirming, isConfirmed, modalMessage]);
-
-  // Save image to IPFS
-  const handleSaveImage = (async (): Promise<string | undefined> => {
-    if (canvasRef.current) {
-      try {
-        const dataURL = canvasRef.current.toDataURL('image/png');
-        const blob = await fetch(dataURL).then((res) => res.blob());
-        const formData = new FormData();
-        formData.append('file', blob, username);
-
-        const response = await fetch('/api/upload', { method: 'POST', body: formData });
-        const data = await response.json();
-        if (response.ok) return data.ipfsHash;
-        else throw new Error(data.message || 'Upload failed.');
-      } catch (error) {
-        console.error("Error uploading image:", error);
-      }
-    }
-  });
-
-  // Handle casting an image
-  const handleCast = useCallback(async () => {
-    const ipfsHash = await handleSaveImage();
-    if (ipfsHash) {
-      sdk.actions.openUrl(
-        `https://warpcast.com/~/compose?text=This%20is%20really%20cool%20-%20Frame%20by%20@joebaeda&embeds[]=https://gateway.pinata.cloud/ipfs/${ipfsHash}`
-      );
-    } else {
-      console.error("Failed to send cast.");
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Handle minting a token
-  const handleMint = async () => {
-    const ipfsHash = await handleSaveImage();
-    if (ipfsHash) {
-      try {
-        writeContract({
-          abi: pixelCastAbi,
-          chainId: base.id,
-          address: pixelCastAddress,
-          functionName: 'mint',
-          value: parseEther('0.001'),
-          args: [`ipfs://${ipfsHash}`],
-        });
-
-      } catch (error) {
-        console.error("Error minting image:", error);
-      }
-    } else {
-      console.error("Failed to upload drawing to IPFS.");
-    }
-  };
-
-  // Clear canvas
-  const handleClearCanvas = () => {
-    if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d');
-      ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-      localStorage.removeItem('pixelArt');
-    }
-  };
-
-  // Load saved art on mount
+  // Load saved art
   useEffect(() => {
     const savedArt = localStorage.getItem('pixelArt');
     if (savedArt && canvasRef.current) {
       const img = new Image();
       img.onload = () => {
-        const ctx = canvasRef.current?.getContext('2d');
-        ctx?.drawImage(img, 0, 0);
+        canvasRef.current?.getContext('2d')?.drawImage(img, 0, 0);
       };
       img.src = savedArt;
     }
   }, []);
 
-  // Handle color change
-  const handleColorChange = (color: string) => setSelectedColor(color);
+  // Transaction Modal Handler
+  useEffect(() => {
+    if (isPending) setTransactionModalMessage('Confirming...');
+    else if (isConfirming) setTransactionModalMessage('Waiting...');
+    else if (isConfirmed) setTransactionModalMessage('Transaction Confirmed! Success 🎉');
+    else setTransactionModalMessage(null);
 
-  return (
+    const timer = setTimeout(() => setTransactionModalMessage(null), 3000);
+    return () => clearTimeout(timer);
+  }, [isPending, isConfirming, isConfirmed]);
+
+  // Load SDK
+  useEffect(() => {
+    const loadSDK = async () => {
+      const sdkContext = await sdk.context;
+      setContext(sdkContext);
+      sdk.actions.ready({});
+    };
+
+    if (!isSDKLoaded) {
+      setIsSDKLoaded(true);
+      loadSDK();
+    }
+  }, [isSDKLoaded]);
+
+  // Prevent render if SDK is not loaded
+  if (!isSDKLoaded) return <Redirect />;
+
+  // Save image to IPFS
+  const handleSaveImage = (async (): Promise<string | undefined> => {
+    if (canvasRef.current && context?.user?.username) {
+      try {
+        const dataURL = canvasRef.current.toDataURL('image/png');
+        const blob = await fetch(dataURL).then((res) => res.blob());
+        const formData = new FormData();
+        formData.append('file', blob, context.user.username);
+
+        const response = await fetch('/api/upload', { method: 'POST', body: formData });
+        const data = await response.json();
+        return response.ok ? data.ipfsHash : undefined;
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  });
+
+  // Handle Cast
+  const handleCast = (async () => {
+    const ipfsHash = await handleSaveImage();
+    if (ipfsHash) {
+      sdk.actions.openUrl(
+        `https://warpcast.com/~/compose?text=This%20is%20really%20cool%20-%20Frame%20by%20@joebaeda&embeds[]=https://gateway.pinata.cloud/ipfs/${ipfsHash}`
+      );
+    }
+  });
+
+  // Handle Mint
+  const handleMint = async () => {
+    const ipfsHash = await handleSaveImage();
+    if (ipfsHash) {
+      writeContract({
+        abi: pixelCastAbi,
+        chainId: base.id,
+        address: pixelCastAddress,
+        functionName: 'mint',
+        value: parseEther('0.001'),
+        args: [`ipfs://${ipfsHash}`],
+      });
+    }
+  };
+
+  // Handle Clear
+  const handleClearCanvas = () => {
+    const ctx = canvasRef.current?.getContext('2d');
+    ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    localStorage.removeItem('pixelArt');
+  };
+
+  return context?.user ? (
     <>
-      <header>
-        <Header username={username} pfp={pfp} />
-      </header>
-      <main>
-        <PixelGrid
-          gridSize={{ width: 48, height: 48 }}
-          selectedColor={selectedColor}
-          canvasRef={canvasRef}
-        />
-        {showColorPicker && (
-          <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
-            <div className="flex space-y-4 flex-col bg-white p-4 rounded-md shadow-lg">
-              <SketchPicker
-                color={selectedColor}
-                onChange={(color) => handleColorChange(color.hex)}
-              />
-              <button
-                onClick={() => setShowColorPicker(false)}
-                className="w-full py-2 rounded-2xl bg-blue-500 text-white text-2xl font-semibold hover:bg-blue-700 transition"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        )}
+      <Header username={context.user.username} pfp={context.user.pfpUrl} />
+      <PixelGrid gridSize={{ width: 48, height: 48 }} selectedColor={selectedColor} canvasRef={canvasRef} />
 
-        {showTransactionModal && modalMessage && (
-          <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
-            <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg">
-              <p className="text-lg font-bold">{modalMessage}</p>
-            </div>
-          </div>
-        )}
+      {/* Color Picker */}
+      {showColorPicker && (
+        <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
+          <SketchPicker color={selectedColor} onChange={(color) => setSelectedColor(color.hex)} />
+          <button
+            onClick={() => setShowColorPicker(false)}
+            className="w-full py-2 bg-blue-500 text-white rounded-2xl"
+          >
+            Close
+          </button>
+        </div>
+      )}
 
-        {/* Toolbar bottom */}
-        <div className="fixed rounded-t-2xl mx-auto bottom-0 left-0 right-0 bg-[#281537] shadow-md p-4">
-          <div className="flex mx-auto flex-row justify-between items-center space-x-4">
-
-            <div className="flex flex-col space-y-1 items-center">
-              <button
-                disabled={isConfirming || isPending}
-                onClick={() => setShowColorPicker(true)}
-                className="rounded-full disabled:opacity-50"
-              >
-                <Palette className="w-6 h-6 text-gray-200" />
-              </button>
-              <p className="text-gray-200 text-sm font-bold">Color</p>
-            </div>
-
-            <div className="flex flex-col space-y-1 items-center">
-              <button
-                disabled={isConfirming || isPending}
-                onClick={handleCast}
-                className="rounded-full disabled:opacity-50"
-              >
-                {isConfirming || isPending ? (
-                  <Ellipsis className="w-6 h-6 text-gray-200 animate-bounce" />
-                ) : (
-                  <CastButton className="w-6 h-6" />
-                )}
-              </button>
-              <p className="text-gray-200 text-sm font-bold">Cast</p>
-            </div>
-
-            <div className="flex flex-col space-y-1 items-center">
-              <button
-                disabled={isConfirming || isPending}
-                onClick={handleMint}
-                className="rounded-full disabled:opacity-50"
-              >
-                {isConfirming || isPending ? (
-                  <Ellipsis className="w-6 h-6 text-gray-200 animate-bounce" />
-                ) : (
-                  <BaseButton className="w-6 h-6" />
-                )}
-              </button>
-              <p className="text-gray-200 text-sm font-bold">Mint</p>
-            </div>
-
-            <div className="flex flex-col space-y-1 items-center">
-              <button
-                disabled={isConfirming || isPending}
-                onClick={handleClearCanvas}
-                className="rounded-full disabled:opacity-50"
-              >
-                <Trash2 className="w-6 h-6 text-gray-200" />
-              </button>
-              <p className="text-gray-200 text-sm font-bold">Delete</p>
-            </div>
-
+      {/* Transaction Modal */}
+      {transactionModalMessage && (
+        <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-900 bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <p className="text-lg font-bold">{transactionModalMessage}</p>
           </div>
         </div>
-      </main>
+      )}
+
+      {/* Toolbar */}
+      <div className="absolute space-x-2 bottom-0 bg-gray-800 p-4 w-full flex justify-around">
+        <Palette onClick={() => setShowColorPicker(true)} />
+        <CastButton onClick={handleCast} />
+        <BaseButton onClick={handleMint} />
+        <Trash2 onClick={handleClearCanvas} />
+      </div>
     </>
+  ) : (
+    <Loading />
   );
 };
 
 export default PixelCast;
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,8 @@
-"use client"
 
-import { useEffect, useState } from 'react';
 import PixelCast from './components/PixelCast'
-import { Redirect } from './components/Redirect';
-import sdk, { FrameContext } from '@farcaster/frame-sdk';
-import Loading from './components/Loading';
 
 export default function Home() {
-
-  const [isSDKLoaded, setIsSDKLoaded] = useState(false)
-  const [context, setContext] = useState<FrameContext>()
-
-  useEffect(() => {
-    const load = async () => {
-      setContext(await sdk.context);
-      sdk.actions.ready({});
-    };
-    if (sdk && !isSDKLoaded) {
-      setIsSDKLoaded(true);
-      load();
-    }
-  }, [isSDKLoaded]);
-
-  if (!isSDKLoaded) {
-    return <Redirect />
-  }
-
   return (
-    <>
-      {context?.client.added ?
-        <PixelCast username={context.user.username as string} pfp={context.user.pfpUrl as string} />
-        : <Loading />
-      }
-      {/*<PixelCast username={"Joe bae"} pfp={"/splash.png"} />*/}
-    </>
+    <PixelCast />
   )
 }
