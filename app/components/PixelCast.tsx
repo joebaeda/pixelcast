f077c5818da958a389847a2f6f076fa72830634f
Joe bae <lineanity@gmail.com>
2024-12-18 19:24:11 +0700

    testing

--- app/components/PixelCast.tsx
+++ app/components/PixelCast.tsx
@@ -45,16 +45,9 @@ const PixelCast = () => {
   }, []);
 
   // Handle Cast
-  const handleCast = async () => {
-    try {
-      const ipfsHash = await handleSaveImage()
-      if (ipfsHash) {
-        sdk.actions.openUrl(`https://warpcast.com/~/compose?text=this%20is%20really%20cool%20-%20just%20create%20one!&embeds[]=https://gateway.pinata.cloud/ipfs/${ipfsHash}`)
-        setNotifOnCast(true)
-      }
-    } catch (error) {
-      console.log(error)
-    }
+  const handleCast = () => {
+      sdk.actions.openUrl("https://warpcast.com/~/compose?text=this%20is%20really%20cool%20-%20just%20create%20one!&embeds[]=https://pixelcast.vercel.app")
+      setNotifOnCast(true)
   }
 
   // Create Notifications
